bec3b19df8979051145fbef223e2d98c57404c0c
emma@beinproximity.com <emma@beinproximity.com>
2022-01-13 13:01:29 -0700

    refactored javascript to only include on for loop to generate random character for the password string

--- script.js
+++ script.js
@@ -12,115 +12,100 @@ var options = {
 
 // Write password to the #password input
 function writePassword() {
-  // generatePassword will return thePassword
+  // GeneratePassword will return the password
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-  console.log (password)
   
   passwordText.value = password;
   }
 
+
 function generatePassword() {
-    // user inputs length of password
+    // User inputs length of password
     var passwordLength = prompt ("Length of password? Between 8-128 digits.")
-  
+
+    // Empty array for enabled options that user chooses
+    var enabledOptions = []
+    // Empty string to pluck random characters from to the length of chosen Passwordlength
+    var passwordString = ""
+     
     // or user cancels
     if (passwordLength === null) { 
       // function ends
       return; 
     }
 
-    // if passwordLength is too short or too long
+    // If passwordLength is too short or too long
     else if (passwordLength < 8 || passwordLength > 128) {
       // confirm error message
       var lengthInvalid = confirm ("You have chosen a password that is too short or too long. \nPlease try again.")
     }
-    
+
+    // If user chooses ok on error message, back to beginning of function    
     if (lengthInvalid === true) {
       generatePassword();
     }
 
     // or if user selects cancel
     else if (lengthInvalid === false) {
-      // end function
+      // function ends
       return; 
     } 
 
-    var enabledOptions = []
-
-    //user chooses to include lowercase letters
+    //User chooses to include lowercase letters
     var lowercase = confirm ("Would you like to include lowercase letters? Choose 'Ok' for yes or 'Cancel' for no.")
     if (lowercase) {
       enabledOptions.push("lowercase");
     }
 
-    // user chooses to include uppercase letters
+    // User chooses to include uppercase letters
     var uppercase = confirm ("Would you like to include uppercase letters? Choose 'Ok' for yes or 'Cancel' for no.")
     if (uppercase) {
       enabledOptions.push("uppercase");
     }
 
-    // user chooses to include numbers
+    // User chooses to include numbers
     var number = confirm ("Would you like to include numbers? Choose 'Ok' for yes or 'Cancel' for no.")
     if (number) {
       enabledOptions.push("number");
     }
 
-    //user chooses to include special characters
+    //User chooses to include special characters
     var symbol = confirm ("Would you like to include special characters? Choose 'Ok' for yes or 'Cancel' for no.")
     if (symbol) {
       enabledOptions.push("symbol");
     }
 
-
-    // error if no character types chosen
+    // Error if no character types chosen
     if (!enabledOptions.length) {
       var confirmError = confirm ("Error: You must choose to include at least one character type.") 
     }
     
+    // User confirms error, function begins again
     if (confirmError){
       generatePassword (); 
     }
-      
-  // initialized blank password to add to it
-  var thePassword = "";
-  // will start at the first enabled option
-  var optionTypeToUsePosition = 0;
+    
 
-  // looping the password as long as the user chose
-  for (let i = 0; i < passwordLength; i++) {
-    // get the name of the option type from the enabled options
-    var optionTypeToUse = enabledOptions[optionTypeToUsePosition];
+    for (let i = 0; i < passwordLength; i++) {
+      // Shuffle enabledOptions to the passwordLength
+      var optionIndex = Math.floor(Math.random() * enabledOptions.length)
+      // Select one random array from optionIndex
+      var optionKey = enabledOptions[optionIndex] 
+      // Shuffle characters within the optionKey
+      var characterIndex = Math.floor(Math.random() * options[optionKey].length)
+      // Genereate a random character from the numerical value that corresponds with a character from the options
+      var randomCharacter = options[optionKey][characterIndex]
 
-    // getting random character from the options for the option type that we're working with
-    var theCharacter = getRandomOptionTypeCharacter(optionTypeToUse);
-
-    //adds theCharacter to the end of thePassword
-    thePassword += theCharacter;
-
-    // resets optionTypeToUsePosition back to first enabled option to choose from options again otherwise go to next enabled option
-    if (optionTypeToUsePosition === enabledOptions.length-1) {
-      optionTypeToUsePosition = 0;
-    } else {
-      optionTypeToUsePosition++;
+      // Add randomCharacter to the end of passwordString
+      passwordString += randomCharacter
     }
-  }
 
-  return thePassword;
-
-}
-
-function getRandomOptionTypeCharacter(optionTypeToUse) {
-  // gets the amount of characters available for this option type
-  var optionTypeTotal = options[optionTypeToUse].length;
-  var randomCharacterPosition = Math.floor(Math.random() * optionTypeTotal);
-
-  // pluck theCharacter from the optionType at the random generated position from above
-  var theCharacter = options[optionTypeToUse][randomCharacterPosition];
-
-  return theCharacter;
-}
-
+    // Return the generated password as the output
+    console.log(enabledOptions)
+    return passwordString;  
+    
+  } // End of generatePassword function
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
